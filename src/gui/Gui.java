bea1630a22c64ed885692cc1e7904979d557780c
nickgoldberg42 <nick.goldberg42@gmail.com>
2017-12-07 19:08:42 -0700

    Finished game actions, reached reset game

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -19,24 +19,31 @@ import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
 import main.btlshyp.message.AttackResponseMessage;
+import main.btlshyp.message.AttackResponseMessage.HitOrMiss;
 import main.btlshyp.model.Coordinate;
 import main.btlshyp.model.Ship;
 import main.btlshyp.view.View;
+import main.btlshyp.view.event.AttackEvent;
 import main.btlshyp.view.event.AttackListener;
 import main.btlshyp.view.event.ChatEvent;
 import main.btlshyp.view.event.ChatListener;
+import main.btlshyp.view.event.SetShipEvent;
 import main.btlshyp.view.event.SetShipListener;
 import net.miginfocom.swing.MigLayout;
 
 public class Gui extends View {
+	private static final long serialVersionUID = 1L;
 	private final JTextField textEntry;
 	JTextArea txtArea;
 	JScrollPane scrollPane;
+	JButton btnAttack;
 	String username = "";
 	ArrayList<Coordinate> newShipCoor;
-	Map<Coordinate, JCheckBox> coorMap = new HashMap<Coordinate, JCheckBox>();
-	ArrayList<Coordinate> yourCoord = new ArrayList<>();
-	ArrayList<Coordinate> oppCoord = new ArrayList<>();
+	Map<Coordinate, JCheckBox> yourCoorMap;
+	Map<Coordinate, JCheckBox> oppCoorMap;
+	ArrayList<Coordinate> yourCoord;
+	ArrayList<Coordinate> oppCoord;
+	Coordinate attackCoor;
 
 	public Gui() {
 		super();
@@ -44,6 +51,12 @@ public class Gui extends View {
 		getContentPane().setLayout(new MigLayout("fill",
 				"[8.00,grow][9.00,grow][grow][][][][][14.00][6.00][8.00][4.00][-27.00][160.00,grow][17.00][73.00]",
 				"[11.00,grow][29.00][][][32.00][50.00,grow][5.00,grow][][][][]"));
+
+		newShipCoor = new ArrayList<Coordinate>();
+		yourCoorMap = new HashMap<Coordinate, JCheckBox>();
+		oppCoorMap = new HashMap<Coordinate, JCheckBox>();
+		yourCoord = new ArrayList<>();
+		oppCoord = new ArrayList<>();
 
 		for (int i = 0; i <= 4; i++) {
 			for (int j = 0; j <= 4; j++) {
@@ -59,209 +72,229 @@ public class Gui extends View {
 
 		JCheckBox checkBox = new JCheckBox("");
 		getContentPane().add(checkBox, "cell 2 1");
-		coorMap.put(oppCoord.get(5), checkBox);
-		checkBox.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				checkBox.setBackground(new Color(0, 0, 0));
-			}
-		});
+		oppCoorMap.put(oppCoord.get(5), checkBox);
 
 		JCheckBox checkBox_1 = new JCheckBox("");
 		getContentPane().add(checkBox_1, "cell 3 1");
-		coorMap.put(oppCoord.get(6), checkBox_1);
+		oppCoorMap.put(oppCoord.get(6), checkBox_1);
 
 		JCheckBox checkBox_2 = new JCheckBox("");
 		getContentPane().add(checkBox_2, "cell 4 1");
-		coorMap.put(oppCoord.get(7), checkBox_2);
+		oppCoorMap.put(oppCoord.get(7), checkBox_2);
 
 		JCheckBox checkBox_3 = new JCheckBox("");
 		getContentPane().add(checkBox_3, "cell 5 1");
-		coorMap.put(oppCoord.get(8), checkBox_3);
+		oppCoorMap.put(oppCoord.get(8), checkBox_3);
 
 		JCheckBox checkBox_4 = new JCheckBox("");
 		getContentPane().add(checkBox_4, "cell 6 1");
-		coorMap.put(oppCoord.get(9), checkBox_4);
+		oppCoorMap.put(oppCoord.get(9), checkBox_4);
 
 		JCheckBox checkBox_5 = new JCheckBox("");
 		getContentPane().add(checkBox_5, "cell 2 2");
-		coorMap.put(oppCoord.get(10), checkBox_5);
+		oppCoorMap.put(oppCoord.get(10), checkBox_5);
 
 		JCheckBox checkBox_6 = new JCheckBox("");
 		getContentPane().add(checkBox_6, "cell 3 2");
-		coorMap.put(oppCoord.get(11), checkBox_6);
+		oppCoorMap.put(oppCoord.get(11), checkBox_6);
 
 		JCheckBox checkBox_7 = new JCheckBox("");
 		getContentPane().add(checkBox_7, "cell 4 2");
-		coorMap.put(oppCoord.get(12), checkBox_7);
+		oppCoorMap.put(oppCoord.get(12), checkBox_7);
 
 		JCheckBox checkBox_8 = new JCheckBox("");
 		getContentPane().add(checkBox_8, "cell 5 2");
-		coorMap.put(oppCoord.get(13), checkBox_8);
+		oppCoorMap.put(oppCoord.get(13), checkBox_8);
 
 		JCheckBox checkBox_9 = new JCheckBox("");
 		getContentPane().add(checkBox_9, "cell 6 2");
-		coorMap.put(oppCoord.get(14), checkBox_9);
+		oppCoorMap.put(oppCoord.get(14), checkBox_9);
 
 		JCheckBox checkBox_10 = new JCheckBox("");
 		getContentPane().add(checkBox_10, "cell 2 0");
-		coorMap.put(oppCoord.get(0), checkBox_10);
+		oppCoorMap.put(oppCoord.get(0), checkBox_10);
 
 		JCheckBox checkBox_11 = new JCheckBox("");
 		getContentPane().add(checkBox_11, "cell 3 0");
-		coorMap.put(oppCoord.get(1), checkBox_11);
+		oppCoorMap.put(oppCoord.get(1), checkBox_11);
 
 		JCheckBox checkBox_12 = new JCheckBox("");
 		getContentPane().add(checkBox_12, "cell 4 0");
-		coorMap.put(oppCoord.get(2), checkBox_12);
+		oppCoorMap.put(oppCoord.get(2), checkBox_12);
 
 		JCheckBox checkBox_13 = new JCheckBox("");
 		getContentPane().add(checkBox_13, "cell 5 0");
-		coorMap.put(oppCoord.get(3), checkBox_13);
+		oppCoorMap.put(oppCoord.get(3), checkBox_13);
 
 		JCheckBox checkBox_14 = new JCheckBox("");
 		getContentPane().add(checkBox_14, "cell 6 0");
-		coorMap.put(oppCoord.get(4), checkBox_14);
+		oppCoorMap.put(oppCoord.get(4), checkBox_14);
 
 		JCheckBox checkBox_15 = new JCheckBox("");
 		getContentPane().add(checkBox_15, "cell 2 3");
-		coorMap.put(oppCoord.get(15), checkBox_15);
+		oppCoorMap.put(oppCoord.get(15), checkBox_15);
 
 		JCheckBox checkBox_16 = new JCheckBox("");
 		getContentPane().add(checkBox_16, "cell 3 3");
-		coorMap.put(oppCoord.get(16), checkBox_16);
+		oppCoorMap.put(oppCoord.get(16), checkBox_16);
 
 		JCheckBox checkBox_17 = new JCheckBox("");
 		getContentPane().add(checkBox_17, "cell 4 3");
-		coorMap.put(oppCoord.get(17), checkBox_17);
+		oppCoorMap.put(oppCoord.get(17), checkBox_17);
 
 		JCheckBox checkBox_18 = new JCheckBox("");
 		getContentPane().add(checkBox_18, "cell 5 3");
-		coorMap.put(oppCoord.get(18), checkBox_18);
+		oppCoorMap.put(oppCoord.get(18), checkBox_18);
 
 		JCheckBox checkBox_19 = new JCheckBox("");
 		getContentPane().add(checkBox_19, "cell 6 3");
-		coorMap.put(oppCoord.get(19), checkBox_19);
+		oppCoorMap.put(oppCoord.get(19), checkBox_19);
 
 		JCheckBox checkBox_20 = new JCheckBox("");
 		getContentPane().add(checkBox_20, "cell 2 4");
-		coorMap.put(oppCoord.get(20), checkBox_20);
+		oppCoorMap.put(oppCoord.get(20), checkBox_20);
 
 		JCheckBox checkBox_21 = new JCheckBox("");
 		getContentPane().add(checkBox_21, "cell 3 4");
-		coorMap.put(oppCoord.get(21), checkBox_21);
+		oppCoorMap.put(oppCoord.get(21), checkBox_21);
 
 		JCheckBox checkBox_22 = new JCheckBox("");
 		getContentPane().add(checkBox_22, "cell 4 4");
-		coorMap.put(oppCoord.get(22), checkBox_22);
+		oppCoorMap.put(oppCoord.get(22), checkBox_22);
 
 		JCheckBox checkBox_23 = new JCheckBox("");
 		getContentPane().add(checkBox_23, "cell 5 4");
-		coorMap.put(oppCoord.get(23), checkBox_23);
+		oppCoorMap.put(oppCoord.get(23), checkBox_23);
 
 		JCheckBox checkBox_24 = new JCheckBox("");
 		getContentPane().add(checkBox_24, "cell 6 4");
-		coorMap.put(oppCoord.get(24), checkBox_24);
+		oppCoorMap.put(oppCoord.get(24), checkBox_24);
 
 		JCheckBox checkBox_49 = new JCheckBox("");
 		getContentPane().add(checkBox_49, "cell 2 6");
-		coorMap.put(yourCoord.get(0), checkBox_49);
+		yourCoorMap.put(yourCoord.get(0), checkBox_49);
 
 		JCheckBox checkBox_25 = new JCheckBox("");
 		getContentPane().add(checkBox_25, "cell 3 6");
-		coorMap.put(yourCoord.get(1), checkBox_25);
+		yourCoorMap.put(yourCoord.get(1), checkBox_25);
 
 		JCheckBox checkBox_26 = new JCheckBox("");
 		getContentPane().add(checkBox_26, "cell 4 6");
-		coorMap.put(yourCoord.get(2), checkBox_26);
+		yourCoorMap.put(yourCoord.get(2), checkBox_26);
 
 		JCheckBox checkBox_27 = new JCheckBox("");
 		getContentPane().add(checkBox_27, "cell 5 6");
-		coorMap.put(yourCoord.get(3), checkBox_27);
+		yourCoorMap.put(yourCoord.get(3), checkBox_27);
 
 		JCheckBox checkBox_28 = new JCheckBox("");
 		getContentPane().add(checkBox_28, "cell 6 6");
-		coorMap.put(yourCoord.get(4), checkBox_28);
+		yourCoorMap.put(yourCoord.get(4), checkBox_28);
 
 		JCheckBox checkBox_29 = new JCheckBox("");
 		getContentPane().add(checkBox_29, "cell 2 7");
-		coorMap.put(yourCoord.get(5), checkBox_29);
+		yourCoorMap.put(yourCoord.get(5), checkBox_29);
 
 		JCheckBox checkBox_30 = new JCheckBox("");
 		getContentPane().add(checkBox_30, "cell 3 7");
-		coorMap.put(yourCoord.get(6), checkBox_30);
+		yourCoorMap.put(yourCoord.get(6), checkBox_30);
 
 		JCheckBox checkBox_31 = new JCheckBox("");
 		getContentPane().add(checkBox_31, "cell 4 7");
-		coorMap.put(yourCoord.get(7), checkBox_31);
+		yourCoorMap.put(yourCoord.get(7), checkBox_31);
 
 		JCheckBox checkBox_32 = new JCheckBox("");
 		getContentPane().add(checkBox_32, "cell 5 7");
-		coorMap.put(yourCoord.get(8), checkBox_32);
+		yourCoorMap.put(yourCoord.get(8), checkBox_32);
 
 		JCheckBox checkBox_33 = new JCheckBox("");
 		getContentPane().add(checkBox_33, "cell 6 7");
-		coorMap.put(yourCoord.get(9), checkBox_33);
+		yourCoorMap.put(yourCoord.get(9), checkBox_33);
 
 		JCheckBox checkBox_34 = new JCheckBox("");
 		getContentPane().add(checkBox_34, "cell 2 8");
-		coorMap.put(yourCoord.get(10), checkBox_34);
+		yourCoorMap.put(yourCoord.get(10), checkBox_34);
 
 		JCheckBox checkBox_35 = new JCheckBox("");
 		getContentPane().add(checkBox_35, "cell 3 8");
-		coorMap.put(yourCoord.get(11), checkBox_35);
+		yourCoorMap.put(yourCoord.get(11), checkBox_35);
 
 		JCheckBox checkBox_36 = new JCheckBox("");
 		getContentPane().add(checkBox_36, "cell 4 8");
-		coorMap.put(yourCoord.get(12), checkBox_36);
+		yourCoorMap.put(yourCoord.get(12), checkBox_36);
 
 		JCheckBox checkBox_37 = new JCheckBox("");
 		getContentPane().add(checkBox_37, "cell 5 8");
-		coorMap.put(yourCoord.get(13), checkBox_37);
+		yourCoorMap.put(yourCoord.get(13), checkBox_37);
 
 		JCheckBox checkBox_38 = new JCheckBox("");
 		getContentPane().add(checkBox_38, "cell 6 8");
-		coorMap.put(yourCoord.get(14), checkBox_38);
+		yourCoorMap.put(yourCoord.get(14), checkBox_38);
 
 		JCheckBox checkBox_39 = new JCheckBox("");
 		getContentPane().add(checkBox_39, "cell 2 9");
-		coorMap.put(yourCoord.get(15), checkBox_39);
+		yourCoorMap.put(yourCoord.get(15), checkBox_39);
 
 		JCheckBox checkBox_40 = new JCheckBox("");
 		getContentPane().add(checkBox_40, "cell 3 9");
-		coorMap.put(yourCoord.get(16), checkBox_40);
+		yourCoorMap.put(yourCoord.get(16), checkBox_40);
 
 		JCheckBox checkBox_41 = new JCheckBox("");
 		getContentPane().add(checkBox_41, "cell 4 9");
-		coorMap.put(yourCoord.get(17), checkBox_41);
+		yourCoorMap.put(yourCoord.get(17), checkBox_41);
 
 		JCheckBox checkBox_42 = new JCheckBox("");
 		getContentPane().add(checkBox_42, "cell 5 9");
-		coorMap.put(yourCoord.get(18), checkBox_42);
+		yourCoorMap.put(yourCoord.get(18), checkBox_42);
 
 		JCheckBox checkBox_43 = new JCheckBox("");
 		getContentPane().add(checkBox_43, "cell 6 9");
-		coorMap.put(yourCoord.get(19), checkBox_43);
+		yourCoorMap.put(yourCoord.get(19), checkBox_43);
 
 		JCheckBox checkBox_44 = new JCheckBox("");
 		getContentPane().add(checkBox_44, "cell 2 10");
-		coorMap.put(yourCoord.get(20), checkBox_44);
+		yourCoorMap.put(yourCoord.get(20), checkBox_44);
 
 		JCheckBox checkBox_45 = new JCheckBox("");
 		getContentPane().add(checkBox_45, "cell 3 10");
-		coorMap.put(yourCoord.get(21), checkBox_45);
+		yourCoorMap.put(yourCoord.get(21), checkBox_45);
 
 		JCheckBox checkBox_46 = new JCheckBox("");
 		getContentPane().add(checkBox_46, "cell 4 10");
-		coorMap.put(yourCoord.get(22), checkBox_46);
+		yourCoorMap.put(yourCoord.get(22), checkBox_46);
 
 		JCheckBox checkBox_47 = new JCheckBox("");
 		getContentPane().add(checkBox_47, "cell 5 10");
-		coorMap.put(yourCoord.get(23), checkBox_47);
+		yourCoorMap.put(yourCoord.get(23), checkBox_47);
 
 		JCheckBox checkBox_48 = new JCheckBox("");
 		getContentPane().add(checkBox_48, "cell 6 10");
-		coorMap.put(yourCoord.get(24), checkBox_48);
+		yourCoorMap.put(yourCoord.get(24), checkBox_48);
+
+		for (Map.Entry<Coordinate, JCheckBox> m : yourCoorMap.entrySet()) {
+			m.getValue().setBackground(Color.BLUE);
+		}
+
+		for (Map.Entry<Coordinate, JCheckBox> m : oppCoorMap.entrySet()) {
+			m.getValue().setBackground(Color.BLUE);
+		}
+
+		for (Map.Entry<Coordinate, JCheckBox> m : yourCoorMap.entrySet()) {
+			m.getValue().addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					newShipCoor.add(m.getKey());
+				}
+			});
+		}
+
+		for (Map.Entry<Coordinate, JCheckBox> m : oppCoorMap.entrySet()) {
+			m.getValue().addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					attackCoor = m.getKey();
+				}
+			});
+		}
 
 		JTextArea spacer = new JTextArea();
 		spacer.setEditable(false);
@@ -272,11 +305,12 @@ public class Gui extends View {
 		scrollPane = new JScrollPane(txtArea);
 		getContentPane().add(scrollPane, "cell 12 0 1 10,grow");
 
-		JButton btnAttack = new JButton("Attack");
+		btnAttack = new JButton("Attack");
 		btnAttack.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				sendAttack(e);
+				attackCoor = null;
 			}
 		});
 		getContentPane().add(btnAttack, "cell 14 8,growx");
@@ -286,6 +320,7 @@ public class Gui extends View {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				attemptSetShip(e);
+				newShipCoor.clear();
 			}
 		});
 		getContentPane().add(btnSetShips, "cell 14 9");
@@ -325,13 +360,28 @@ public class Gui extends View {
 
 	@Override
 	public void attemptSetShip(ActionEvent e) {
-		super.attemptSetShip(e);
+		ArrayList<Coordinate> newList = new ArrayList<>(newShipCoor);
+		Ship ship = shipToPlace;
+		ship.setShipCoordinates(newList);
+		SetShipEvent sse = new SetShipEvent(e, ship);
+		if (setShipListener != null) {
+			setShipListener.setShipEventOccurred(sse);
+		}
+		for (Coordinate c : newShipCoor) {
+			yourCoorMap.get(c).setBackground(new Color(0, 0, 0));
+			System.out.println(c.x + " " + c.y);
+		}
 	}
 
 	@Override
 	public void displayAttack(AttackResponseMessage message) {
-		// TODO Auto-generated method stub
-		super.displayAttack(message);
+		txtArea.append("Your attack was a " + message.getHitOrMiss().toString() + " at ("
+				+ String.valueOf(message.getCoordinate().x) + ", " + String.valueOf(message.getCoordinate().y) + ")");
+		if (message.getHitOrMiss() == HitOrMiss.HIT) {
+			oppCoorMap.get(message.getCoordinate()).setBackground(Color.RED);
+		} else {
+			oppCoorMap.get(message.getCoordinate()).setBackground(Color.WHITE);
+		}
 	}
 
 	@Override
@@ -350,8 +400,9 @@ public class Gui extends View {
 
 	@Override
 	public void displayOpponentAttack(AttackResponseMessage message) {
-		// TODO Auto-generated method stub
-		super.displayOpponentAttack(message);
+		txtArea.append("Opponent fired a " + message.getHitOrMiss().toString() + " at ("
+				+ String.valueOf(message.getCoordinate().x) + ", " + String.valueOf(message.getCoordinate().y) + ")");
+		yourCoorMap.get(message.getCoordinate()).setBackground(Color.RED);
 	}
 
 	@Override
@@ -368,8 +419,13 @@ public class Gui extends View {
 
 	@Override
 	public void notYourTurn() {
-		// TODO Auto-generated method stub
-		super.notYourTurn();
+		txtArea.append("Waiting on opponent...");
+		try {
+			Thread.sleep(1500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		btnAttack.setEnabled(false);
 	}
 
 	@Override
@@ -392,14 +448,31 @@ public class Gui extends View {
 
 	@Override
 	public void resetGame() {
-		// TODO Auto-generated method stub
-		super.resetGame();
+		for (Map.Entry<Coordinate, JCheckBox> m : yourCoorMap.entrySet()) {
+			m.getValue().setBackground(Color.BLUE);
+			m.getValue().setSelected(false);
+		}
+
+		for (Map.Entry<Coordinate, JCheckBox> m : oppCoorMap.entrySet()) {
+			m.getValue().setBackground(Color.BLUE);
+			m.getValue().setSelected(false);
+		}
+		txtArea.append("\n\n***  Restarting game ......");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		txtArea.setText("");
 	}
 
 	@Override
 	public void sendAttack(ActionEvent e) {
-		// TODO Auto-generated method stub
-		super.sendAttack(e);
+		Coordinate coordinate = attackCoor;
+		AttackEvent ae = new AttackEvent(e, coordinate);
+		if (attackListener != null) {
+			attackListener.attackEventOccurred(ae);
+		}
 	}
 
 	@Override
@@ -415,11 +488,13 @@ public class Gui extends View {
 	@Override
 	public void setShip(Ship ship) {
 		txtArea.append("Please choose " + ship.getShipSize() + " spaces to place your " + ship.getShipType().toString());
+		shipToPlace = ship;
 	}
 
 	@Override
 	public void yourTurn() {
-		super.yourTurn();
+		txtArea.append("It is your turn...");
+		btnAttack.setEnabled(true);
 	}
 
 }
